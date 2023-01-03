db69d1afcbf63801fb3f8255568938c136b52ad1
JoeydeJonge <jd.dejonge@student.avans.nl>
2023-01-03 16:34:23 +0100

    Good upgrade

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,7 +1,10 @@
-import express from 'express'
+const express = require('express');
 const {generateImage, askQuestion} = require('../controllers/openaiController');
 const router = express.Router();
 
 router.post('/generateimage', generateImage);
+
+router.post('/', askQuestion)
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // Enable body parser
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get('/', async (req, res) => {
+    res.status(200).send({
+      message: 'Hello from JoeyAI!'
+    })
+  })
+  
+  
 app.use('/openai', require('./routes/openaiRoutes'))
 
-app.listen(port, () => console.log(`Server started on ${port}`));
+app.listen(port, () => console.log('JoeyAI server started on http://localhost:5000'));
 
